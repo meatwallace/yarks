8ce86d4634323fbe32340dcd3eb7da7e41d1b9e4
Geoff Whatley <me@geoffwhatley.com>
2019-08-03 12:00:54 +1000

    fix(git): remove hardcoded ref from push function

--- packages/core/src/releaseWorkspaces.ts
+++ packages/core/src/releaseWorkspaces.ts
@@ -615,7 +615,7 @@ async function pushChanges(workspace, options, env) {
 async function pushGitRef(ref, options, env) {
   let result = await options.git.push({
     dir: options.cwd,
-    ref: workspace.currentRelease,
+    ref,
     token: env.GITHUB_TOKEN,
     // TODO(#26): accomodate other git hosts and other repository URL formats
     url: options.repositoryURL,
